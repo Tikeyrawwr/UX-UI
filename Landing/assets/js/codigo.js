008b851cc6a4156f32c40cd284e8fed805fc0cd3
Tikeyrawwr <60155388+Tikeyrawwr@users.noreply.github.com>
2021-03-31 19:16:33 -0300

    Add files via upload

---
+++ Landing/assets/js/codigo.js
@@ -0,0 +1,37 @@
+@keyframes MessageOn{
+    0%{
+      background:transparent;
+      color:transparent;
+    }
+    25% {
+      background:transparent;
+      color:transparent;
+    }
+    50%{
+      background: black;
+      color: white;
+    }
+    100%{
+      background:transparent;
+      color:transparent;
+    }
+  
+  }
+  
+  @keyframes MouthChange{
+    0%{
+      height:110px;
+      width: 7px;
+      border-radius:3px;
+    }
+    50%{
+      height: 150px;
+      width: 75px;
+      border-radius: 160px 0 0 160px;
+    }
+    100%{
+      height:110px;
+      width: 7px;
+      border-radius:3px;
+    }
+  }
